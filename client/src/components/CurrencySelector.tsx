e6c4095838691fbb7a78090b82e1f18cd9c7795f
Engin Sarak <258687380+EnginSarak@users.noreply.github.com>
2026-04-27 16:09:30 +0200

    Update CurrencySelector.tsx

--- client/src/components/CurrencySelector.tsx
+++ client/src/components/CurrencySelector.tsx
@@ -249,7 +249,7 @@ function CurrencySelector({ language, onLanguageChange }: Props) {
             <img
               src="/tabi-logo-horizontal.svg"
               alt="Tabi"
-              className="h-13 mx-auto"
+              className="h-[52px] mx-auto w-auto"
               style={{ filter: "brightness(0) invert(1) opacity(0.9)" }}
               draggable={false}
             />
